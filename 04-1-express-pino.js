daa36d2760c94c5ed213a51b017ce02f6a78b12c
RafaelGSS <rafael.nunu@hotmail.com>
2021-12-16 11:33:54 -0300

    update: use pino destination with async logging

--- 04-1-express-pino.js
+++ 04-1-express-pino.js
@@ -6,9 +6,8 @@ const pino = require('pino')
 const dest = pino.destination({ sync: false }) // logs to stdout with no args
 const logger = pino(dest)
 
-app.use(require('express-pino-logger')({ logger }))
-
 app.get('/', function (req, res) {
+  logger.info(req)
   res.send('hello world')
 })
 
